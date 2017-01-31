78f34d55a2886d8e7d3f31f8ba9d21dbafc462f5
Daniel Lefever <lefeverde@pitt.edu>
2017-01-31 14:16:11 -0500

    made it cli executable

--- chromo_gc_content.py
+++ chromo_gc_content.py
@@ -13,6 +13,9 @@ from __future__ import division
 
 import argparse
 import os
+import sys
+import csv
+
 from Bio import SeqIO
 from Bio.SeqUtils import GC
 #from util_functions import *
@@ -29,12 +32,71 @@ args = parser.parse_args()
 
 
 def main():
-    chrom_seqrecs = []
+    header = (
+    'chromosome_id,' +
+    'gene_name,' +
+    'gene_id,' +
+   # 'gi_num,' +
+    'start_up_gc,' +
+    'start_down_gc,' +
+    'end_up_gc,' +
+    'end_down_gc,' +
+    'start_up_seq,' +
+    'start_down_seq,' +
+    'end_up_seq,' +
+    'end_down_seq'
+    )
+
+    file_dir =  os.path.dirname(os.path.abspath(args.i))
+
+    raw_dir = os.path.join(file_dir, 'raw_csvs')
+    if not os.path.exists(raw_dir):
+        os.makedirs(raw_dir)
+
+    unique_dir = os.path.join(file_dir, 'unique_csvs')
+    if not os.path.exists(unique_dir):
+        os.makedirs(unique_dir)
+
+    #chrom_seqrecs = []
     for seqrec in SeqIO.parse(open(args.i), 'gb'):
-        gc_writer(seqrec)
+        if seqrec.id.split('_')[0] == 'NC':
+            gc_writer(seqrec, raw_dir, header)
+
+    unique_cds_maker(raw_dir, unique_dir, header)
+    concat_list = [i.split(',') for i in concat_list_maker(unique_dir, header)]
+    with open('concat_file.csv', 'w+') as cf:
+        cf_writer = csv.writer(cf)
+        #cf_writer.writerows((concat_list.split(',')))
+        cf_writer.writerows((concat_list))
+
+def unique_cds_maker(raw_dir, unique_dir, header):
+    header = header + '\n'
+    for raw_cnt_csv in os.listdir(raw_dir):
+        unique_cds_set = set()
+        with open(os.path.join(raw_dir,raw_cnt_csv)) as cur_f:
+            next(cur_f)
+            for line in cur_f:
+                unique_cds_set.add(line)
 
 
-def gc_writer(parsed_gb, feature_string = 'CDS', consec_bases = 750):
+        unique_list = [(i.strip('\r\n').split(',')) for i in unique_cds_set]
+        cur_file_handle = os.path.join(unique_dir, (raw_cnt_csv.split('.')[0] + '_unique_feats.csv'))
+
+        with open(cur_file_handle, 'w+') as ind_unique_csv:
+            ind_unique_csv.write(header)
+
+            #ind_csv_writer = csv.writer(ind_unique_csv, lineterminator='\n')
+            ind_csv_writer = csv.writer(ind_unique_csv)
+
+            ind_csv_writer.writerows(unique_list)
+
+
+
+
+
+
+
+def gc_writer(parsed_gb, out_dir, header, feature_string = 'CDS', consec_bases = 750):
     '''
     This function finds all the features delineated by the feature_string.
     The defualt is for CDS, and four regions with default size 750 bp
@@ -45,48 +107,27 @@ def gc_writer(parsed_gb, feature_string = 'CDS', consec_bases = 750):
 
     '''
     chromo_id = parsed_gb.id
-    cur_handle = str(chromo_id) + '.csv'
-
+    cur_handle = os.path.join(out_dir, (str(chromo_id) + '.csv'))
+    str_seq = str(parsed_gb.seq)
     with open(cur_handle, 'w+') as f:
-        f.write(
-            'gene_name,' +
-            'gene_id,' +
-           # 'gi_num,' +
-            'start_up_gc,' +
-            'start_down_gc,' +
-            'end_up_gc,' +
-            'end_down_gc,' +
-            'start_up_seq,' +
-            'start_down_seq,' +
-            'end_up_seq,' +
-            'end_down_seq,' +
-            '\n'
-         )
+        f.write(header)
 
         cds_seqrec_list = feature_getter(parsed_gb, feature_string)
-        for feature_seqrec in cds_seqrec_list:
+        for feature_num, feature_seqrec in enumerate(cds_seqrec_list):
+            if feature_num % 1000 == 0:
+                sys.stdout.write((chromo_id + ' ') + '{0}\r'.format(feature_num),)
+                sys.stdout.flush()
             gene_name = feature_seqrec.qualifiers['gene'][0]
             gene_id = gene_id_finder(feature_seqrec)
-
-            '''
-            try:
-                gene_id = feature_seqrec.qualifiers['db_xref'][2].split(':')[1]
-                if 'HGNC' in gene_id:
-                    continue
-            except IndexError:
-                continue
-            '''
-
-
-            #gi_num = feature_seqrec.qualifiers['db_xref'][0].split(':')[1]
             start_upseq, start_downseq, end_upseq, end_downseq = \
-            seq_streams(parsed_gb, feature_seqrec, consec_bases)
-            start_up_gc = GC(start_upseq.seq)
-            start_down_gc = GC(start_downseq.seq)
-            end_up_gc = GC(end_upseq.seq)
-            end_down_gc = GC(end_downseq.seq)
+            seq_streams(str_seq, feature_seqrec, consec_bases)
+            start_up_gc = GC(start_upseq)
+            start_down_gc = GC(start_downseq)
+            end_up_gc = GC(end_upseq)
+            end_down_gc = GC(end_downseq)
 
             f.write(
+                str(chromo_id) + ',' +
                 str(gene_name) + ',' +
                 str(gene_id) + ',' +
                 #str(gi_num) + ',' +
@@ -94,10 +135,10 @@ def gc_writer(parsed_gb, feature_string = 'CDS', consec_bases = 750):
                 str(start_down_gc) + ',' +
                 str(end_up_gc) + ',' +
                 str(end_down_gc) + ',' +
-                str(start_upseq.seq) + ',' +
-                str(start_downseq.seq) + ',' +
-                str(end_upseq.seq) + ',' +
-                str(end_downseq.seq) + '\n'  #+ ',' +
+                str(start_upseq) + ',' +
+                str(start_downseq) + ',' +
+                str(end_upseq) + ',' +
+                str(end_downseq) + '\n'  #+ ',' +
                 )
 
 
@@ -149,6 +190,64 @@ def feature_getter(parsed_gb, feature_string='CDS', spec_gene=None):
                     cds_seqs.append(extracted_feature)
                     #cds_seqs.append((extracted_feature,feature_seqrec))
     return(cds_seqs)
+
+def unique_cds_getter(raw_count_path):
+    with open('concat_unique_cds.csv', 'w+') as concat_file:
+        concat_file.write(
+            'chromosome_id,' +
+            'gene_name,' +
+            'gene_id,' +
+           # 'gi_num,' +
+            'start_up_gc,' +
+            'start_down_gc,' +
+            'end_up_gc,' +
+            'end_down_gc,' +
+            'start_up_seq,' +
+            'start_down_seq,' +
+            'end_up_seq,' +
+            'end_down_seq,' +
+            '\n'
+        )
+
+        for cur_csv in os.listdir(raw_count_path):
+            #print cur_csv
+            if cur_csv.split('.')[-1] == 'csv':
+                cur_file_list = [line for line in open(os.path.join(raw_count_path,cur_csv), 'rU')]
+                unique_cds = list(set(cur_file_list))
+                #return unique_cds
+                #unique_cds_with_chromo_id = [cur_csv.split('.')[0] + i for i in unique_cds]
+                cur_file_handle = (cur_csv.split('.')[0] + '_unique_feats.csv')
+                #return unique_cds
+                with open(cur_file_handle, 'w+') as ind_unique_csv:
+                    ind_csv_writer = csv.writer(ind_unique_csv)
+                    ind_csv_writer.writerows((unique_cds.strip('\r\n').split(',')))
+
+
+def concat_list_maker(unique_cds_dir, header):
+    concat_list = []
+    concat_list.append(header)
+
+    for cur_csv in os.listdir(unique_cds_dir):
+        if cur_csv.split('.')[-1] == 'csv':
+            #cur_csv_list = [i.strip('\r') for i in open(os.path.join(unique_cds_dir,cur_csv))]
+            cur_csv_list = [i.strip('\r\n') for i in open(os.path.join(unique_cds_dir,cur_csv))]
+            #cur_csv_list = [i for i in open(os.path.join(unique_cds_dir,cur_csv))]
+        try:
+            for cur_line in cur_csv_list[1:]:
+                concat_list.append(cur_line)
+                #concat_list.append((cur_line.strip('\"\"')))
+                #concat_list.append((cur_line.strip('\n')))
+        except UnboundLocalError:
+            continue
+
+    return concat_list
+
+
+
+
+
+
+
 
 
 if __name__ == '__main__':
